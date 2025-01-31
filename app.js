8a3d83f8dc7c4604863521b37a0c8ddad9ff3af9
Paul Zamora <PolZam0ra@outlook.com>
2025-01-31 10:24:00 -0600

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo() {
+    let amigoAleatorio = document.getElementById('amigo').value;
+    if (amigoAleatorio.trim() === '') {
+        alert('Error, debe ingresar un nombre');
+    }else if (/\d/.test(amigoAleatorio)){
+        alert('Error, debe ingresar un nombre y no numeros');
+    }else{
+        amigos.push(amigoAleatorio);
+        console.log(amigos);
+        alert(`Amigo agregado: ${amigoAleatorio}`);
+        document.getElementById('amigo').value = "";
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length === null) {
+        alert('No hay amigos agregados');
+    } else {
+        let amigoAleatorio = amigos[Math.floor(Math.random() * amigos.length)];
+        alert(`Amigo aleatorio: ${amigoAleatorio}`);
+        document.querySelector('#amigo').removeAttribute('disabled');
+        document.getElementById('sortear').setAttribute('disabled', 'disabled');
+        amigos = amigos.filter(amigo => amigo!== amigoAleatorio);
+    }
+}
